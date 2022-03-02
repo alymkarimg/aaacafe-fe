b01080dea1230f3f9d4f066cb45ef74b0cb4e492
farhanjamil259 <farhanjamil259@gmail.com>
2022-03-02 20:37:08 +0500

    test: commit

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -6,6 +6,8 @@ type LayoutProps = {
   children: React.ReactElement;
 };
 
+const func = () => {};
+
 const Layout = ({ children }: LayoutProps): React.ReactElement => {
   return (
     <div className="container">
